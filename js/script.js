c2929a261b324958abbdd6d93c73b703ccf52c2d
Mariia <64638931+MariiaFish@users.noreply.github.com>
2022-11-10 14:03:27 +0300

    rebuild

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ const map = L.map("map").setView(
 
 // Добавляем слой с картой из OpenStreetMap
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
+  // maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
